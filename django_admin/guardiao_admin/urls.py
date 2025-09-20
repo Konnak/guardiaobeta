78eb2812c4200518e07de8259286e2028c2d9b98
konnak <cleisondecarvalho@hotmail.com>
2025-09-20 15:31:32 -0300

    fix: corrigir URLs Django - remover referências a views inexistentes

--- django_admin/guardiao_admin/urls.py
+++ django_admin/guardiao_admin/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/discord-login/', include('guardiao.urls')),
     path('login/', LoginView.as_view(template_name='admin/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('guardiao/', include('guardiao.urls')),
 ]
 
 # Serve static and media files in development
